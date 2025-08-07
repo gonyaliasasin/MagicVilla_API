4ce9e4963c5f0aa6bc3bec3fe31c30ad937b1a67
Abdullah OĞUL <ogul.abdullah@tkteknoloji.com.tr>
2025-08-07 10:58:47 +0300

    deneme1

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,117 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+[Route("api/VillaAPI")]
+[ApiController]
+public class VillaAPIController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+    {
+        return VillaStore.villaList;
+    }
+
+
+    [HttpGet("{id:int}", Name = "GetVilla")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<VillaDTO> GetVilla(int id)
+    {
+
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        if (villa == null)
+        {
+            return NotFound();
+        }
+            
+        return Ok(villa);
+    }
+
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+    {
+
+        if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+        {
+            ModelState.AddModelError("Custom Error", "Villa already exists!");
+            return BadRequest(ModelState);
+        }
+
+        if (villaDTO == null)
+        {
+            return BadRequest(villaDTO);
+        }
+
+        if (villaDTO.Id > 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+        VillaStore.villaList.Add(villaDTO);
+
+        return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+    }
+
+
+    [HttpDelete("{id:int}", Name = "DeleteVilla")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteVilla(int id)
+    {
+
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        if (villa == null)
+        {
+            return NotFound();
+        }
+
+        VillaStore.villaList.Remove(villa);
+        return NoContent();
+
+    }
+
+
+    [HttpPut("{id:int}", Name = "UpdateVilla")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+    {
+        
+        if (villaDTO == null || id != villaDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        villa.Name = villaDTO.Name;
+        villa.Sqft= villaDTO.Sqft;
+        villa.Occupancy = villaDTO.Occupancy;
+
+        return NoContent();
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data;
+
+public  static class VillaStore
+{
+    public static List<VillaDTO> villaList = new List<VillaDTO>
+    {
+        new VillaDTO{Id=1,Name = "Mountain View", Sqft = 400, Occupancy = 4},
+        new VillaDTO{Id=2,Name = "Ocean View", Sqft = 300, Occupancy = 3},
+        new VillaDTO{Id=3,Name = "Island View", Sqft = 200, Occupancy = 2},
+        new VillaDTO{Id=4,Name = "Lake View", Sqft = 100, Occupancy = 1}
+    };
+}
