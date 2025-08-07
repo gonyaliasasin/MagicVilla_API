fc37212c1113a0724fbf858fd8526adeeb27bc31
gonyaliasasin <abdullahoul42@gmail.com>
2025-08-07 13:38:58 +0300

    deneme

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
@@ -112,6 +113,34 @@ public class VillaAPIController : ControllerBase
         villa.Name = villaDTO.Name;
         villa.Sqft= villaDTO.Sqft;
         villa.Occupancy = villaDTO.Occupancy;
+
+        return NoContent();
+    }
+
+
+    [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+    {
+        if (patchDTO == null || id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        if (villa == null)
+        {
+            return BadRequest();
+        }
+        
+        patchDTO.ApplyTo(villa, ModelState);
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
 
         return NoContent();
     }
