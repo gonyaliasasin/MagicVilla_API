11188ed90fb45d6c2bde033fa487826048a3c97f
gonyaliasasin <abdullahoul42@gmail.com>
2025-08-11 16:17:20 +0300

    deneme

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -12,18 +12,18 @@ namespace MagicVilla_VillaAPI.Controllers;
 public class VillaAPIController : ControllerBase
 {
 
-    private readonly ILogger<VillaAPIController> _logger;
+    //private readonly ILogger<VillaAPIController> _logger;
 
-    public VillaAPIController(ILogger<VillaAPIController> logger)
-    {
-        _logger = logger;
-    }
+    //public VillaAPIController(ILogger<VillaAPIController> logger)
+    //{
+    //    _logger = logger;
+    //}
 
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<IEnumerable<VillaDTO>> GetVillas()
     {
-        _logger.LogInformation("Getting all villas");
+        //_logger.LogInformation("Getting all villas");
         return VillaStore.villaList;
     }
 
@@ -37,7 +37,7 @@ public class VillaAPIController : ControllerBase
 
         if (id == 0)
         {
-            _logger.LogError("Get Villa Error with Id" + id);
+            //_logger.LogError("Get Villa Error with Id " + id);
             return BadRequest();
         }
 
